88527092810d544667310d584c00b447baa90ab1
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-03 23:48:11 +0600

    Refactor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './Redux/store'
 import './styles/bootstrap.min.css'
+
 import './styles/styles.css'
 
 ReactDOM.render(
